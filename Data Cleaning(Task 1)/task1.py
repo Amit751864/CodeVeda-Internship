dfad810f4ab3bb118b640eab47a0d2814bc26dad
Amit Kumar Gupta <kumarguptaa254@gmail.com>
2026-07-26 19:53:11 +0530

    Clean up task1.py by removing comments and whitespace
    Removed excessive comments and whitespace from task1.py.

--- Data Cleaning(Task 1)/task1.py
+++ Data Cleaning(Task 1)/task1.py
@@ -1,26 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv("churn-bigml-80.csv")
-print(df.head()) # iska kaam ha ki  first 5 row print
-print(df.shape) # ya (row ,column )
-print(df.columns) # ya tumko columns ka naam print karega 
-print(df.info()) # ya tumko tumhare data ka summary provide karega
-print(df.isnull().sum()) # isme apko batya ga ki kitna null value haa 0
-print(df.isnull().sum().sum()) # isme pura calculate kar ke bata thaaa haa
-print(df.duplicated())# iska matlab ha ki false  = no duplicate rows, but true give dupliccate row in your data
-print(df.duplicated().sum()) # ya pura count kar ke bata haa
-print(df.describe()) #isme stastic bata ha mean = count, mean, std, min . 25%,50%,75%,max
-print(df['Account length'].median()) # ya function are provide  are mid value  in column are account length
+print(df.head()) 
+print(df.shape) 
+print(df.columns) 
+print(df.info()) 
+print(df.isnull().sum()) 
+print(df.isnull().sum().sum()) 
+print(df.duplicated())
+print(df.duplicated().sum()) 
+print(df.describe()) 
+print(df['Account length'].median()) 
 print(df.columns.tolist())
 print(df['Account length'].mean())
-print(df['State'].mode()) # ya bta ha ki kitne kio column me reapat hua haa
-print(df['State'].unique()) # ya function are provide kitne unique value haa
-print(df['State'].nunique())  # ya function are provide are total unique value
-print(df['State'].value_counts()) # ya kitne bar repeat huaaa ya batya gaa
-print(df['State'].describe()) # count     2666
-# unique      51
-# top         WV
-# freq        88
+print(df['State'].mode()) 
+print(df['State'].unique()) 
+print(df['State'].nunique())  
+print(df['State'].value_counts()) 
+print(df['State'].describe()) 
+
 
 
 
